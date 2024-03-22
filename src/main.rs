01e7bad4a5c8c63eb039dc9d87f9146a6b0636bd
Nithe14 <imnithe@gmail.com>
2024-03-22 11:00:52 +0100

    code refactor
    fix sentences state display
    refactor some functions
    users don't see os errors anymore

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod book;
 mod requests;
 
 use actix_files as fs;
-use actix_files::NamedFile;
 use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
 use actix_web::{
     cookie::Key, get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result,
@@ -15,7 +14,6 @@ use std::collections::HashMap;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::BufReader;
-use std::io::Error as IoError;
 use std::path::PathBuf;
 use tera::{Context, Tera};
 
@@ -29,45 +27,46 @@ fn load_data_from_file(file_path: &str) -> Result<Vec<Book>, Box<dyn std::error:
     Ok(data)
 }
 
-async fn get_entry(
-    data: &web::Data<Vec<Book>>,
-    n: usize,
-) -> Result<Book, Box<dyn std::error::Error>> {
-    let books = &data;
-    if n >= books.len() {
-        return Err(Box::new(IoError::new(
+async fn get_entry(data: &Vec<Book>, n: usize) -> Result<Book, Box<dyn std::error::Error>> {
+    if let Some(book) = data.get(n) {
+        Ok(book.clone())
+    } else {
+        Err(Box::new(std::io::Error::new(
             std::io::ErrorKind::Other,
             format!("Out of index [{}]! Book not found.", n),
-        )));
+        )))
     }
-    Ok(books[n].clone())
 }
 
 fn get_template(template: &str, context: Context) -> Result<String, Box<dyn std::error::Error>> {
-    let mut tera =
-        Tera::parse("templates/pl/*").inspect_err(|e| eprintln!("Parsing error: {}", e))?;
-    tera.build_inheritance_chains()?;
+    let tera = Tera::new("templates/pl/*").map_err(|e| {
+        eprintln!("Parsing error: {}", e);
+        Box::new(e) as Box<dyn std::error::Error>
+    })?;
     Ok(tera.render(template, &context)?)
 }
 
-fn parse_render<W, E>(render: Result<String, W>) -> Result<HttpResponse, E>
+fn parse_render<E>(render: Result<String, impl Display>) -> Result<HttpResponse, E>
 where
-    W: Display,
     E: Display,
 {
-    match render {
-        Ok(r) => Ok(HttpResponse::Ok().body(r)),
-        Err(e) => {
-            eprintln!("{}", e);
-            Ok(HttpResponse::InternalServerError().body("500 Internal Server Error"))
-        }
-    }
+    render.map(|r| HttpResponse::Ok().body(r)).or_else(|e| {
+        eprintln!("{}", e);
+        Ok(HttpResponse::InternalServerError().body("500 Internal Server Error"))
+    })
 }
 
 #[get("/")]
-async fn index(_req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = "./static/index.html".parse().unwrap();
-    Ok(NamedFile::open(path)?)
+async fn index(_req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: PathBuf = "./static/index.html".parse()?;
+    let file_result = fs::NamedFile::open(path).map_err(|e| {
+        eprintln!("{}", e);
+        actix_web::error::InternalError::new(
+            "404 Not Found",
+            actix_web::http::StatusCode::NOT_FOUND,
+        )
+    })?;
+    Ok(file_result)
 }
 
 #[get("/api/counter")]
@@ -81,7 +80,7 @@ async fn counter(data: web::Data<Vec<Book>>, session: Session) -> Result<HttpRes
         session.insert("counter", 0)?;
     }
 
-    let book_counter = session.get::<usize>("counter")?.unwrap();
+    let book_counter = session.get::<usize>("counter")?.unwrap_or_default();
     Ok(HttpResponse::Ok()
         .insert_header(("HX-Trigger", "newBook"))
         .body(format!("{}/10", book_counter + 1)))
@@ -94,24 +93,23 @@ async fn render_index(
     params: web::Query<NextReq>,
 ) -> Result<HttpResponse, Error> {
     let mut context = Context::new();
+    let db_size = data.len();
     let count = session.get::<usize>("counter")?.unwrap_or(0);
 
-    if count + 1 >= data.len() && params.next.unwrap_or(false)
+    if count + 1 >= db_size && params.next.unwrap_or(false)
         || session
-            .get(&format!("book_{}_done", &data.len() - 1))?
+            .get(&format!("book_{}_done", &db_size - 1))?
             .unwrap_or(false)
     {
-        //let mut books_points = Vec::new();
         let mut books = HashMap::new();
         let all_points = session.get::<f32>("all_points")?.unwrap_or(0.00);
-        let progress = (all_points / (data.len() as f32 * 5.00)) * 100.00;
+        let progress = (all_points / (db_size as f32 * 5.00)) * 100.00;
         context.insert("progress", &progress);
         context.insert("all_points", &all_points);
-        for book_number in 0..data.len() {
+        for book_number in 0..db_size {
             let points = session
                 .get::<f32>(format!("points_{}", book_number).as_str())?
                 .unwrap_or(0.00);
-            //books_points.push(points);
             let book = get_entry(&data, book_number).await?;
             let key = format!("{} \"{}\"", book.author, book.title);
             books.insert(key, points);
@@ -120,7 +118,6 @@ async fn render_index(
         let render = get_template("finish.html", context);
         return parse_render(render);
     }
-    let db_size = data.len();
     if let Some(count) = session.get::<usize>("counter")? {
         if count < db_size && params.next.unwrap_or(false) {
             session.insert("counter", count + 1)?;
@@ -170,7 +167,6 @@ async fn render_index(
         }
     }
 
-    let _ = session.insert("sentences_state", 1);
     let all_points = session.get::<f32>("all_points")?.unwrap_or(0.00);
     context.insert("all_points", &all_points);
     let progress = (all_points / (data.len() as f32 * 5.00)) * 100.00;
@@ -185,6 +181,7 @@ async fn give_up(data: web::Data<Vec<Book>>, session: Session) -> Result<HttpRes
     let count = session.get::<usize>("counter")?.unwrap_or(0);
     session.insert(format!("points_{}", count.to_string()), 0)?;
     session.insert(format!("book_{}_done", count), true)?;
+    let _ = session.insert("sentences_state", 1);
     let all_points = session.get::<f32>("all_points")?.unwrap_or(0.00); //no points adding
     let book: Book = match get_entry(&data, count).await {
         Ok(value) => value,
@@ -234,6 +231,7 @@ async fn check_book(
         let is_done: bool = session
             .get::<bool>(format!("book_{}_done", count).as_str())?
             .unwrap_or(false);
+        let _ = session.insert("sentences_state", 1);
         if is_done == false {
             session.insert(format!("points_{}", count.to_string()), current_points)?;
             session.insert(format!("book_{}_done", count), true)?;
